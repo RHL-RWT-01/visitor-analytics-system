74b2ddddc24b8a5be6df5afb7c709708be830da2
Rahul Rawat <rahulrawat4060@gmail.com>
2025-08-03 23:40:54 +0530

    utilty FNs

---
+++ server/utils/broadcast.js
@@ -0,0 +1,7 @@
+export function broadcast(wss, message) {
+  wss.clients.forEach((client) => {
+    if (client.readyState === client.OPEN) {
+      client.send(JSON.stringify(message));
+    }
+  });
+}
